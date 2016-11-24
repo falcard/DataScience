3a28fb831d4110a1f2346dbc57943ddbc207bbae
falcard <logan@falcard.com>
2016-11-23 19:53:05 -0600

    initial commit

---
+++ Unit 11 Assignment/Analysis/SetupEnvironment.R
@@ -0,0 +1,13 @@
+pkg <- c("downloader","repmis","dplyr","ggplot2", "fpp")
+new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+if (length(new.pkg)){
+  install.packages(new.pkg, dependencies = TRUE,repos='http://cran.us.r-project.org')}
+sapply(pkg, require, character.only = TRUE)
+
+# 
+# library(downloader)
+# library(repmis)
+# library(tidyr)
+# library(dplyr)
+# library(ggplot2)
+# library(fpp)
